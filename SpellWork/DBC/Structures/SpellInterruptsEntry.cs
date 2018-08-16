6b27a4e0b5f710e00d4dbf7204b104e4895520de
Смердокрыл <35412834+Smerdokryl@users.noreply.github.com>
2018-08-16 17:32:06 +0300

    Update SpellInterruptsEntry.cs

--- SpellWork/DBC/Structures/SpellInterruptsEntry.cs
+++ SpellWork/DBC/Structures/SpellInterruptsEntry.cs
@@ -5,8 +5,8 @@
         public uint ID;
         public byte DifficultyID;
         public short InterruptFlags;
-        public int AuraInterruptFlags[MAX_SPELL_AURA_INTERRUPT_FLAGS];
-        public int ChannelInterruptFlags[MAX_SPELL_AURA_INTERRUPT_FLAGS];
+        public int[] AuraInterruptFlags;
+        public int[] ChannelInterruptFlags;
         public int SpellID;
     }
 }
